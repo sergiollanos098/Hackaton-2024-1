4506eb72e0c5c55a4bfe2adeeed38c31352388e4
SergioLlanos <sergio.llanos@utec.edu.pe>
2024-05-21 14:18:26 -0500

    commit

---
+++ src/main/java/com/example/hackaton2024/Hackaton2024Application.java
@@ -0,0 +1,13 @@
+package com.example.hackaton2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackaton2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hackaton2024Application.class, args);
+    }
+
+}
